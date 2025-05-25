14cd26c92156ba7a79cac2526ab38b7d808aca2f
adam <adam@rosenblatt.ca>
2025-05-25 12:35:32 -0400

    added create account activity that links from main activity

--- app/src/main/java/com/example/localloop/MainActivity.java
+++ app/src/main/java/com/example/localloop/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.localloop;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToAccountCreation(View view) {
+        //Launch CreateAccountActivity
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 
     public void ValidateCredentials (View view) {
@@ -47,13 +51,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void HandleInvalidCredentials (View view) {
         // clear username and password fields
-        final TextView clearUser = (TextView) findViewById(R.id.username_input);
+        final TextView clearUser = findViewById(R.id.username_input);
         clearUser.setText("");
-        final TextView clearPass = (TextView) findViewById(R.id.password_input);
+        final TextView clearPass = findViewById(R.id.password_input);
         clearPass.setText("");
 
         // state bad credentials
-        final TextView badCreds = (TextView) findViewById(R.id.badcreds_state);
+        final TextView badCreds = findViewById(R.id.badcreds_state);
         badCreds.setText("Invalid credentials, please try again.");
     }
 
